77d9b1a3f677a22eb0bdc35b15c0d3e0185d1127
lpalfonso <lpalfonso@unal.edu.co>
2025-12-16 21:57:22 -0500

    test for sam using telperion

--- tests/test_sam.py
+++ tests/test_sam.py
@@ -1,18 +1,18 @@
 import unittest
-import hylite
-from hylite import io
+import BlackTelperion
+from BlackTelperion import io
 from pathlib import Path
 import numpy as np
 import os
 
 class MyTestCase(unittest.TestCase):
     def test_sam(self):
-        from hylite.analyse.sam import SAM, spectral_angles
+        from BlackTelperion.analyse.sam import SAM, spectral_angles
 
         image = io.load(os.path.join(os.path.join(str(Path(__file__).parent.parent), "test_data"),"image.hdr"))
-        cloud = io.load(os.path.join(os.path.join(str(Path(__file__).parent.parent), "test_data"),"hypercloud.hdr"))
+        #cloud = io.load(os.path.join(os.path.join(str(Path(__file__).parent.parent), "test_data"),"hypercloud.hdr"))
 
-        for data in [image, cloud]:
+        for data in  [image]:#[image, cloud]:
             em1 = data.X(onlyFinite=True)[0]
             em2 = data.X(onlyFinite=True)[-1]
 
@@ -24,14 +24,14 @@ class MyTestCase(unittest.TestCase):
             em3 = data.X(onlyFinite=True)[-10]
             sam = SAM(data, [[em3], [em1,em2] ])
             self.assertTrue(np.isfinite(sam.data).any())
-            self.assertEquals( int( sam.X( onlyFinite=True )[0,0]), 1 )
-            self.assertEquals(int(sam.X(onlyFinite=True)[-1, 0]), 1 )
-            self.assertEquals(int(sam.X(onlyFinite=True)[-10, 0]), 0 )
+            self.assertEqual( int( sam.X( onlyFinite=True )[0,0]), 1 )
+            self.assertEqual(int(sam.X(onlyFinite=True)[-1, 0]), 1 )
+            self.assertEqual(int(sam.X(onlyFinite=True)[-10, 0]), 0 )
 
             # run SAM with a library
             arr = np.vstack( [em3,em2] )[:,None,:]
-            lib = hylite.HyLibrary( arr, lab=['EM1','EM2'], wav=data.get_wavelengths() )
+            lib = BlackTelperion.BlackLibrary( arr, lab=['EM1','EM2'], wav=data.get_wavelengths() )
             sam = SAM(data, lib)
             self.assertTrue(np.isfinite(sam.data).any())
-            self.assertEquals(int(sam.X(onlyFinite=True)[-1, 0]), 1 )
-            self.assertEquals(int(sam.X(onlyFinite=True)[-10, 0]), 0 )
+            self.assertEqual(int(sam.X(onlyFinite=True)[-1, 0]), 1 )
+            self.assertEqual(int(sam.X(onlyFinite=True)[-10, 0]), 0 )
